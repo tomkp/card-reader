9f1f873bd651a3d2eb635c65a58c2f54dbc97180
tomkp <tom@tomkp.com>
2016-03-08 19:16:09 +0000

    buffer handling

--- src/card-reader.js
+++ src/card-reader.js
@@ -86,6 +86,11 @@ device.issueCommand = (command, fn) => {
     } else if (typeof command === 'string') {
         console.log('command is a String', command);
         buffer = new Buffer(hexify.toByteArray(command));
+    } else if (Buffer.isBuffer(command)) {
+        console.log('command is a Buffer', command);
+        buffer = command;
+    } else {
+        throw 'Unable to recognise command type';
     }
 
     var protocol = 1;
