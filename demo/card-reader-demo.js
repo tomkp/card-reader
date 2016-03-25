fb17ced83faba9d878770323c31ae3f0fc567225
tomkp <tom@tomkp.com>
2016-03-25 21:07:21 +0000

    handle multiple readers

--- demo/card-reader-demo.js
+++ demo/card-reader-demo.js
@@ -1,39 +1,39 @@
 'use strict';
 
-let cardreader = require('../lib/card-reader');
+let devices = require('../lib/card-reader');
 
-cardreader.on('device-activated', function (event) {
-    console.log(`Device '${event.reader.name}' activated, devices: ${cardreader.listDevices()}`);
+devices.on('device-activated', function (event) {
+    console.log(`Device '${event.reader.name}' activated, devices: ${devices.listDevices()}`);
 });
 
-cardreader.on('device-deactivated', function (event) {
-    console.log(`Device '${event.reader.name}' deactivated, devices: ${cardreader.listDevices()}`);
+devices.on('device-deactivated', function (event) {
+    console.log(`Device '${event.reader.name}' deactivated, devices: ${devices.listDevices()}`);
 });
 
-cardreader.on('card-removed', function (event) {
+devices.on('card-removed', function (event) {
     console.log(`Card removed from '${event.reader.name}' `);
 });
 
-cardreader.on('command-issued', function (event) {
+devices.on('command-issued', function (event) {
     console.log(`Command '${event.command}' issued to '${event.reader.name}' `);
 });
 
-cardreader.on('response-received', function (event) {
+devices.on('response-received', function (event) {
     console.log(`Response '${event.response}' received from '${event.reader.name}' in response to '${event.command}'`);
 });
 
-cardreader.on('error', function (event) {
+devices.on('error', function (event) {
     console.log(`Error '${event.error}' received`);
 });
 
-cardreader.on('card-inserted', function (event) {
+devices.on('card-inserted', function (event) {
 
-    console.log(`devices: ${cardreader.listDevices()}`);
+    console.log(`devices: ${devices.listDevices()}`);
 
     var reader = event.reader;
     console.log(`Card inserted into '${reader.name}' `);
 
-    cardreader
+    devices
         .issueCommand(reader, '00A404000E315041592E5359532E4444463031')
         .then(function (response) {
             console.log(`Response '${response.toString('hex')}`);
